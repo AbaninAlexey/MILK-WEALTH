2ef12115bf8bc4749ea5b35e5f7260ad315df80a
Abanin Alexey <abanin_alexey@mail.ru>
2025-03-09 11:06:59 +0700

    tabs on product page

---
+++ scripts/owl.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $('.stories-slider').owlCarousel({
+        responsive: {
+          360: {
+            margin: 0,
+            items: 4,
+          },
+          639: {
+            items: 6,
+            margin: 20,
+          },
+        },
+        nav: true,
+        dots: false,
+        'stageClass': 'stories-slider__stage',
+        'stageOuterClass': 'stories-slider__outer',
+    });
+  });
+
+
+  $(document).ready(function(){
+    $('.cloud-slider').owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.reviews__slider').owlCarousel({
+      responsive: {
+        360: {
+          margin: 0,
+          items: 1,
+          margin: 30,
+        },
+        640: {
+          items: 2,
+          margin: 30,
+        },
+      },
+        nav: false,
+        dots: false,
+        loop: true,
+        autoWidth: true,
+        stagePadding: 362,
+    });
+  });
+
